dca8867398303aa8fddabb9ba40049785e5e237e
alexandre <alexandre.olvrd@eduge.ch>
2023-10-02 11:01:57 +0200

    test api countries

---
+++ js/index.js
@@ -0,0 +1,65 @@
+/**
+ * Projet : Activité Pays
+ * Auteur : Alexandre.Olvrd
+ * Date : 25.09.23
+ **/
+"use strict";
+
+const API_URL = "https://restcountries.com/v3.1/all";
+
+// 10 minutes en millisecondes
+const REFRESH_DELAY = 600000;
+
+async function fetchCountries() {
+    let response = await fetch(API_URL);
+    return response;
+}
+
+/**
+ * Fonction de comparaison pour trier les pays par ordre alphabétique
+ * @param {Object} a Pays 1
+ * @param {Object} b Pays 2
+ * @returns la comparaison des deux pays
+ */
+function compareByName(a, b) {
+    return a.translations.fra.common.localeCompare(b.translations.fra.common);
+}
+
+function getData() {
+    {
+        fetchCountries()
+            .then(response => response.text().then(text => localStorage.setItem("countries", text)))
+            .catch(err => console.error(err));
+    }
+}
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/sw.js");
+    })
+}
+
+if (localStorage.getItem("lastUpdate") === null || localStorage.getItem("countries") === null || Date.now() - localStorage.getItem("lastUpdate") > REFRESH_DELAY) {
+    localStorage.setItem("lastUpdate", Date.now());
+    getData();
+}
+
+let htmlData = "";
+
+let countries = JSON.parse(localStorage.getItem("countries")).sort(compareByName);
+
+for (let c of countries) {
+    htmlData += `
+        <div>
+            <img src="${c.flags.png}" alt="${c.translations.fra.common}">
+            <h1>${c.translations.fra.common}</h1>
+            <p>Population : ${c.population}</p>
+        </div>
+    `;
+}
+
+let minLastUpdate = Math.floor((Date.now() - localStorage.getItem("lastUpdate")) / 60000);
+
+document.querySelector("#lastUpdate").innerHTML = `Dernière mise à jour: il y a ${minLastUpdate} min.`;
+
+document.querySelector("section").innerHTML = htmlData;
--- sw.js
+++ sw.js
@@ -54,3 +54,7 @@ self.__precacheManifest = [
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+self.addEventListener("fetch", e => {
+  console.log(`[Service worker] Fetched ressource ${e.request.url}`);
+});
